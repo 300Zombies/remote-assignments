289649f74c521664582eac7a6e96f7751548c490
300Spartans <TNDark@gmail.com>
2019-06-26 03:26:40 +0800

    week-3-XMLHttpRequest

--- week-3/index.js
+++ week-3/index.js
@@ -7,22 +7,32 @@ function sum(number) {
     return result = ((1 + realNumber) * realNumber) / 2;
 }
 
+/*** Allow CORS ***/
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+/*** on top of any other middleware ***/
+
 app.get("/", (req, res) => {
     res.send("Hello appworks school!");
 });
 
 app.get("/getData", (req, res) => {
     let number = req.query.number;
+    let parsedNumber = parseFloat(number);
     if (!number) {
         res.send("Lack of Parameter.<br>Please enter an integer.")
-    } else if (parseFloat(number) === NaN || parseFloat(number) % 1 !== 0) {
-        // the float type is not a valid input too
-        res.send(`Wrong Parameter.<br>${number} is not an integer.`)
-    } else if (parseFloat(number) !== NaN && parseFloat(number) % 1 === 0) {
+    } else if (parsedNumber === NaN || parsedNumber % 1 !== 0 || parsedNumber < 0) {
+        // rule out float and negative integer
+        res.send(`Wrong Parameter.<br>${number} is not a positive integer.`)
+    } else if (parsedNumber !== NaN && parsedNumber % 1 === 0) {
         res.send(`You entered ${number}.<br>The result is ${String(sum(number))}.`)
     }
 });
 
+app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log(`server launched at port: ${port}`)
